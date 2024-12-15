e260f5a41763ac88453cd4ed633f8f5f805b79c6
GRISHANA <Grisha13082005@gmail.com>
2024-12-15 15:49:20 +0400

    Hello world

--- app/src/main/java/com/example/medkarta/MainActivity.java
+++ app/src/main/java/com/example/medkarta/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.medkarta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +15,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+
+    // Предопределенные логин и пароль
+    private static final String CORRECT_USERNAME = "123";
+    private static final String CORRECT_PASSWORD = "123";
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -19,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
+
         });
+
+            editTextUsername = findViewById(R.id.editTextTextEmailAddress);
+            editTextPassword = findViewById(R.id.editTextTextPassword);
+            buttonLogin = findViewById(R.id.button);
+
+            buttonLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String username = editTextUsername.getText().toString();
+                    String password = editTextPassword.getText().toString();
+
+                    // Проверка логина и пароля
+                    if (username.equals(CORRECT_USERNAME) && password.equals(CORRECT_PASSWORD)) {
+                        // Переход на другую активность
+                        Intent intent = new Intent(MainActivity.this, MainActivityList.class);
+                        startActivity(intent);
+                    } else {
+                        // Сообщение об ошибке
+                        Toast.makeText(MainActivity.this, "Неверный логин или пароль", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            });
     }
 }
+
---
+++ app/src/main/java/com/example/medkarta/MainActivityList.java
@@ -0,0 +1,35 @@
+package com.example.medkarta;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivityList extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView textView = new TextView(this);
+        textView.setText("Добро пожаловать!");
+        textView.setTextSize(24);
+        setContentView(textView);
+
+    }
+
+}
