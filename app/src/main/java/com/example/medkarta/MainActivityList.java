bf9d2392a57097b0dce854b1cf8727d19b8a7703
GRISHANA <Grisha13082005@gmail.com>
2024-12-16 16:38:32 +0400

    ListView ПРивет мир

--- app/src/main/java/com/example/medkarta/MainActivityList.java
+++ app/src/main/java/com/example/medkarta/MainActivityList.java
@@ -1,6 +1,9 @@
 package com.example.medkarta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,6 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivityList extends AppCompatActivity {
 
 
+        private ListView listView;
+        private String[] items = {"Щербанов Алексей Евгеньевич", "Ворапаев Николай Иванович"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +29,25 @@ public class MainActivityList extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+                listView = findViewById(R.id.List);
 
-        TextView textView = new TextView(this);
-        textView.setText("Добро пожаловать!");
-        textView.setTextSize(24);
-        setContentView(textView);
+        // Создание адаптера и привязка его к ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
 
+        // Установка слушателя нажатия на элемент списка
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Переход на DetailActivity с передачей выбранного элемента
+                Intent intent = new Intent(MainActivityList.this, FirstActivity.class);
+                intent.putExtra("selected_item", items[position]);
+                startActivity(intent);
+            }
+        });
     }
 
 }
